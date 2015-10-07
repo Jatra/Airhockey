156cfae37fc08d17e47a8c6a87a3caa7dce0418a
Tim Russell <trussell@zipcar.co.uk>
2015-10-07 07:24:16 +0100

    First commit

---
+++ app/src/main/java/uk/co/jatra/airhockey/AirHockeyRenderer.java
@@ -0,0 +1,174 @@
+package uk.co.jatra.airhockey;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import uk.co.jatra.airhockey.util.TextResourceReader;
+
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.GL_FLOAT;
+import static android.opengl.GLES20.GL_TRIANGLES;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glDrawArrays;
+import static android.opengl.GLES20.glEnableVertexAttribArray;
+import static android.opengl.GLES20.glGetAttribLocation;
+import static android.opengl.GLES20.glGetUniformLocation;
+import static android.opengl.GLES20.glUniform4f;
+import static android.opengl.GLES20.glUseProgram;
+import static android.opengl.GLES20.glVertexAttribPointer;
+import static android.opengl.GLES20.glViewport;
+import static uk.co.jatra.airhockey.util.ShaderHelper.compileFragmentShader;
+import static uk.co.jatra.airhockey.util.ShaderHelper.compileVertexShader;
+import static uk.co.jatra.airhockey.util.ShaderHelper.linkProgram;
+import static uk.co.jatra.airhockey.util.ShaderHelper.validateProgram;
+
+/**
+ * Created by tim on 06/10/2015.
+ */
+public class AirHockeyRenderer implements GLSurfaceView.Renderer {
+    private static final int POSITION_COMPONENT_COUNT = 2;
+    private final float[] tableVerticesWithTriangles = {
+            // Triangle 1
+            0f, 0f,
+            9f, 14f,
+            0f, 14f,
+            // Triangle 2
+            0f, 0f,
+            9f, 0f,
+            9f, 14f,
+
+            // Line 1
+            0f, 7f,
+            9f, 7f,
+            // Mallets
+            4.5f, 2f,
+            4.5f, 12f
+    };
+
+    private static final int BYTES_PER_FLOAT = 4;
+    private final FloatBuffer vertexData;
+    private final String fragmentShaderSource;
+    private final String vertexShaderSource;
+    private int program;
+
+    private static final String U_COLOR = "u_Color";
+    private int uColorLocation;
+    private static final String A_POSITION = "a_Position";
+    private int aPositionLocation;
+
+    public AirHockeyRenderer(Context context) {
+        vertexData = ByteBuffer
+                .allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        vertexData.put(tableVerticesWithTriangles);
+        vertexShaderSource = TextResourceReader
+                .readTextFileFromResource(context, R.raw.simple_vertex_shader);
+        fragmentShaderSource = TextResourceReader
+                .readTextFileFromResource(context, R.raw.simple_fragment_shader);
+
+    }
+
+    /**
+     * Called when the surface is created or recreated.
+     * <p/>
+     * Called when the rendering thread
+     * starts and whenever the EGL context is lost. The EGL context will typically
+     * be lost when the Android device awakes after going to sleep.
+     * <p/>
+     * Since this method is called at the beginning of rendering, as well as
+     * every time the EGL context is lost, this method is a convenient place to put
+     * code to create resources that need to be created when the rendering
+     * starts, and that need to be recreated when the EGL context is lost.
+     * Textures are an example of a resource that you might want to create
+     * here.
+     * <p/>
+     * Note that when the EGL context is lost, all OpenGL resources associated
+     * with that context will be automatically deleted. You do not need to call
+     * the corresponding "glDelete" methods such as glDeleteTextures to
+     * manually delete these lost resources.
+     * <p/>
+     *
+     * @param gl     the GL interface. Use <code>instanceof</code> to
+     *               test if the interface supports GL11 or higher interfaces.
+     * @param config the EGLConfig of the created surface. Can be used
+     */
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        int vertexShader = compileVertexShader(vertexShaderSource);
+        int fragmentShader = compileFragmentShader(fragmentShaderSource);
+        program = linkProgram(vertexShader, fragmentShader);
+        validateProgram(program);
+        glUseProgram(program);
+
+        uColorLocation = glGetUniformLocation(program, U_COLOR);
+        aPositionLocation = glGetAttribLocation(program, A_POSITION);
+
+        vertexData.position(0);
+        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT,
+                false, 0, vertexData);
+        glEnableVertexAttribArray(aPositionLocation);
+
+    }
+
+    /**
+     * Called when the surface changed size.
+     * <p/>
+     * Called after the surface is created and whenever
+     * the OpenGL ES surface size changes.
+     * <p/>
+     * Typically you will set your viewport here. If your camera
+     * is fixed then you could also set your projection matrix here:
+     * <pre class="prettyprint">
+     * void onSurfaceChanged(GL10 gl, int width, int height) {
+     * gl.glViewport(0, 0, width, height);
+     * // for a fixed camera, set the projection too
+     * float ratio = (float) width / height;
+     * gl.glMatrixMode(GL10.GL_PROJECTION);
+     * gl.glLoadIdentity();
+     * gl.glFrustumf(-ratio, ratio, -1, 1, 1, 10);
+     * }
+     * </pre>
+     *
+     * @param gl     the GL interface. Use <code>instanceof</code> to
+     *               test if the interface supports GL11 or higher interfaces.
+     * @param width
+     * @param height
+     */
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+
+    /**
+     * Called to draw the current frame.
+     * <p/>
+     * This method is responsible for drawing the current frame.
+     * <p/>
+     * The implementation of this method typically looks like this:
+     * <pre class="prettyprint">
+     * void onDrawFrame(GL10 gl) {
+     * gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+     * //... other gl calls to render the scene ...
+     * }
+     * </pre>
+     *
+     * @param gl the GL interface. Use <code>instanceof</code> to
+     *           test if the interface supports GL11 or higher interfaces.
+     */
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+    }
+}
